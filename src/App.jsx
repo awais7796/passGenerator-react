efeeb80dadeb37f94b11815fdc5669a42f33ce9c
awais7796 <awaitzz49@gmail.com>
2025-01-21 23:39:16 +0530

    minor changes in ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,48 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
  <>
- <div className="flex h-screen justify-content-center " style={{backgroundColor:"red"}} >
-  <div></div>
-  <div>
-    <input type="range" />
-    <label htmlFor="checkbox">Length</label>
-    <input type="checkbox"  />
-    
-  </div>
- </div>
+ <div 
+      className='flex items-center justify-center h-screen'
+        style={{backgroundColor:""}} >
+
+<div className=' border-4 border-pink-900'>
+        <div className='border-none overflow w-auto '>
+        <input className='p-2' type="text" placeholder='your Password is generating '/>
+        <button>Copy</button>
+        </div>
+        <div className='border-none overflow w-auto '>
+        <input className='p-2' type="text" placeholder='your Password is generating '/>
+        <button>Copy</button>
+        </div>
+        <div className='border-none overflow w-auto '>
+        <input className='p-2' type="text" placeholder='your Password is generating '/>
+        <button>Copy</button>
+        </div>
+        <div className='border-none overflow w-auto '>
+        <input className='p-2' type="text" placeholder='your Password is generating '/>
+        <button>Copy</button>
+        </div>
+        <div className='border-none overflow w-auto '>
+        <input className='p-2' type="text" placeholder='your Password is generating '/>
+        <button>Copy</button>
+        </div>
+
+        <div className='
+        flex gap-4
+        '>
+          <input type="range" />
+          <label htmlFor="checkbox">Length</label>
+          <input type="checkbox"  />
+          <label > Number</label>
+          <input type="checkbox"  />
+          <label > Special Characters</label>
+        </div>
+</div>
+
+</div>
  
  </>
   )
